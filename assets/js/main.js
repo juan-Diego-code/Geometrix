af1c03d91df7f246ba754a9ed5fcf485523e1e90
Juan Diego <juandcortesa11@gmail.com>
2026-07-23 18:26:18 -0500

    Reparacion de bugs y correcion de uft-8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//1) Menú celular
+//1) Menu celular
 
 const menuToggle = document.getElementById('menuBtn');
 const sidebar = document.getElementById('sidebar');
@@ -9,32 +9,32 @@ menuToggle.addEventListener('click', function () {
 
 //2) Calculadoras
 
-// Cuadrado: área = lado * lado
+// Cuadrado: area = lado * lado
 document.getElementById('calcularCuadrado').addEventListener('click', function () {
   const lado = Number(document.getElementById('lado').value);
   const area = lado * lado;
-  document.getElementById('resultadoCuadrado').textContent = 'Área: ' + area;
+  document.getElementById('resultadoCuadrado').textContent = 'Area: ' + area;
 });
 
-// Triángulo: área = (base * altura) / 2
+// Triangulo: area = (base * altura) / 2
 document.getElementById('calcularTriangulo').addEventListener('click', function () {
   const base = Number(document.getElementById('base').value);
   const altura = Number(document.getElementById('altura').value);
   const area = (base * altura) / 2;
-  document.getElementById('resultadoTriangulo').textContent = 'Área: ' + area;
+  document.getElementById('resultadoTriangulo').textContent = 'Area: ' + area;
 });
 
-// Círculo: área = π * radio^2
+// Circulo: area = n * radio^2
 document.getElementById('calcularCirculo').addEventListener('click', function () {
   const radio = Number(document.getElementById('radio').value);
   const area = Math.PI * radio * radio;
-  document.getElementById('resultadoCirculo').textContent = 'Área: ' + area.toFixed(2);
+  document.getElementById('resultadoCirculo').textContent = 'Area: ' + area.toFixed(2);
 });
 
-// Rombo: área = (diagonal1 * diagonal2) / 2
+// Rombo: area = (diagonal1 * diagonal2) / 2
 document.getElementById('calcularRombo').addEventListener('click', function () {
   const d1 = Number(document.getElementById('diagonal1').value);
   const d2 = Number(document.getElementById('diagonal2').value);
   const area = (d1 * d2) / 2;
-  document.getElementById('resultadoRombo').textContent = 'Área: ' + area;
+  document.getElementById('resultadoRombo').textContent = 'Area: ' + area;
 });
